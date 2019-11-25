c816edb3f827ad859c48870034411d0ee5b507c1
svkote <36902132+svkote@users.noreply.github.com>
2019-11-25 13:09:50 +0300

    Update 6. Строки и текст.py

---
+++ ch1/6. Строки и текст.py
@@ -0,0 +1,21 @@
+types_of_people = 10
+x = f"Существует {types_of_people} типов людей."
+
+binary = "Python"
+do_not = "нет"
+y = f"Те, кто понимает {binary}, и те, кто {do_not}."
+
+print(x)
+print(y)
+
+print(f"Я сказал: {x}")
+print(f"А еще я сказал: '{y}'")
+
+hilarious = False
+joke_evaluation = "Разве это смешно? {}"
+print(joke_evaluation.format(hilarious))
+
+w = "Это часть строки слева... "
+e = "Это часть строки справа"
+
+print(w + e)
