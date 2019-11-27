1627ab33b0df157fe76de2ceec70f9beeeb9f726
svkote <36902132+svkote@users.noreply.github.com>
2019-11-27 16:47:12 +0300

    Update ex15.py

--- 15/ex15.py
+++ 15/ex15.py
@@ -1,10 +1,15 @@
+#-*- coding: UTF-8 -*-
 from sys import argv
 
+# передаем в консоли значения для переменной filename
 script, filename = argv
 
+# присваиваем переменной txt действие открыть файл filename
 txt = open(filename)
 
+
 print(f"Содержимое файла: {filename}")
+# Выводим содержимое файла в консоль
 print(txt.read())
 
 print("Снова введите имя файла: ")
