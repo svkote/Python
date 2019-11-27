611556c04010234bd0e48d55aa4d616b47f50557
svkote <36902132+svkote@users.noreply.github.com>
2019-11-27 14:09:25 +0300

    Create 13.py

---
+++ ch2/13.py
@@ -0,0 +1,9 @@
+from sys import argv
+
+script, first, second, third = argv
+print("Этот сценарий называется: ", script)
+print("Моя первая переменная называется: ", first)
+print("Моя вторая переменная называется: ", second)
+print("Моя третья переменная называется: ", third)
+
+# Запуск в консоли: python 13-argv.py first 2nd 3rd
