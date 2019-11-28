f48662102c86c05cf9fbca2de8376d9fbe8f5896
svkote <36902132+svkote@users.noreply.github.com>
2019-11-28 13:53:35 +0300

    Update 19.py

---
+++ 19/19.py
@@ -0,0 +1,14 @@
+def cheese_and_crackers(cheese_count, boxes_of_crackers):
+    print(f"У нас есть {cheese_count} сырков и {boxes_of_crackers} пачек чипсов!")
+
+
+cheese_and_crackers(20, 30)
+
+
+amount_of_cheese = 10
+amount_of_crackers = 50
+cheese_and_crackers(amount_of_cheese, amount_of_crackers)
+
+
+cheese_and_crackers(10 + 10, 20 + 100)
+cheese_and_crackers(amount_of_cheese + 100, 500 + amount_of_crackers)
