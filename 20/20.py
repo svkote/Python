e6598496f2c37b2903c8435e0b609cc6eaa85cb1
svkote <36902132+svkote@users.noreply.github.com>
2019-11-28 14:59:03 +0300

    Update 20.py

---
+++ 20/20.py
@@ -0,0 +1,29 @@
+# from sys import argv
+#
+# script, input_file = argv
+input_file = "test.txt"
+
+
+def print_all(f):
+    print(f.read())
+
+
+def rewind(f):
+    f.seek(0)
+
+
+def print_a_line(line_count, f):
+    print(line_count, f.readline())
+
+
+current_file = open(input_file)
+
+print("Первым делом выведем этот файл целиком: \n")
+print_all(current_file)
+
+print("Теперь отмотаем назад, словно это кассета.")
+rewind(current_file)
+
+print("Выведем три строки:")
+for i in range(1, 4):
+    print_a_line(i, current_file)
