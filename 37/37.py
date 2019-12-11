d4a64f9c4770f21af6491e5c2d0374b7dcb2699e
svkote <36902132+svkote@users.noreply.github.com>
2019-12-11 12:17:55 +0300

    Delete 37.py

--- 37/37.py
+++
@@ -1,6 +0,0 @@
-
-with open('test.txt', 'wt', encoding='UTF-8') as InFile:
-    num = int(input("Введите число: "))
-    line = str('1 / ' + str(num) + ' = ' + str(1 / num))
-    print(line)
-    InFile.write(line)
