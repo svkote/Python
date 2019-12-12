0ae138a761631a7bc727d721bcf24f2b8e334dd8
svkote <36902132+svkote@users.noreply.github.com>
2019-12-12 12:06:42 +0300

    Update 43.py

--- 43/43.py
+++ 43/43.py
@@ -1,15 +1,29 @@
+from sys import exit
+from random import randint
+from textwrap import dedent
+
+
 class Scene(object):
     def enter(self):
-        pass
+        print("Эта сцена еще не настроена."
+              "Создайте подкласс и реализуйте функцию enter()")
+        exit(1)
 
 
 class Engine(object):
     """Движок"""
     def __init__(self, scene_map):
-        pass
+        self.scene_map = scene_map
 
     def play(self):
-        pass
+        current_scene = self.scene_map.opening_scene()
+        last_scene = self.scene_map.next_scene('finished')
+
+        while current_scene != last_scene:
+            next_scene_name = current_scene.enter()
+            current_scene = self.scene_map.next_scene(next_scene_name)
+
+        current_scene.enter()
 
 
 class Death(Scene):
